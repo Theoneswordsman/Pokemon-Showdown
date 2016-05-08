c99bbddc35d42503fa17a9bf470813ed2485ff13
Theoneswordsman <harpreetdhaliwal2525@gmail.com>
2016-05-08 15:08:13 -0700

    Update friendslist.js

--- chat-plugins/friendslist.js
+++ chat-plugins/friendslist.js
@@ -200,7 +200,7 @@ exports.commands = {
 				}
 		}
 	},
-	friendslisthelp: ["Gold's friendslist allows users to add friends to their friendslists. The commands include...",
+	friendslisthelp: ["friendslist allows users to add friends to their friendslists. The commands include...",
 					"/friendslist add, [user] - Adds a user to your friendslist.",
 					"/friendslist remove, [user] - Removes a user from your friendslist.",
 					"/friendslist removeall - Clears your friendslist.",
